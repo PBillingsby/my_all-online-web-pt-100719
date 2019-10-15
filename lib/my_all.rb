797930c441e1d93cf424374c75ffea4633375024
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-15 14:36:53 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -26,8 +26,11 @@ my_all?([1,2,3]) {|i| i < 3}
 
 def my_all?(collection)
   i = 0
+  block_return_values = []
   while i < collection.length
-    yield(collection[i])
+    block_return_values << yield(collection[i])
     i += 1
   end
+  
+  block_return_values.include?(false) ? false : true
 end
