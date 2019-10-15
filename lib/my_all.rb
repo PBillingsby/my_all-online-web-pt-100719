238e8ee09ed3789fe7023508cf35d0f15a8ca50d
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-15 14:34:39 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,5 @@
 require 'pry'
-
+=begin
 def my_all?(collection)
   i = 0
   block_return_values = []
@@ -22,3 +22,5 @@ end
 my_all?([1,2,3]) {|i| i < 3}
 =======
 >>>>>>> c477acfdf840f59d548e0d4f112ce4fbd53daaa5
+=end
+
